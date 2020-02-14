7d88141168eb5194bcb06cd0813ec10fb7095170
Luke Osgood <lukemosgood@gmail.com>
2020-02-14 13:49:05 -0500

    hello component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.scss';
 import Header from './components/Header/Header';
+import Hello from './components/Hello/Hello';
 import Education from './components/Education/Education';
 import Experience from './components/Experience/Experience';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className='container'>
       <Header />
+      <Hello />
       <Education />
       <Experience />
     </div>
---
+++ src/components/Hello/Hello.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Hello.scss';
+
+const Hello = () => {
+	return (
+		<div className='box hello'>
+			<h3>Hello</h3>
+			<p>
+        Excited by and knowledgable about many vital technologies of the web such as HTML, CSS, Javascript, React, Git and more. Always challenging myself to grow as a developer and find ways to solve difficult problems. With the capacity to adapt and learn fast.
+			</p>
+		</div>
+	);
+};
+
+
+export default Hello;
