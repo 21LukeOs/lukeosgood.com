3b35d0054a220d97744bf77b694f763f344e20c9
Luke Osgood <lukemosgood@gmail.com>
2020-02-14 14:56:43 -0500

    code link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,6 +10,14 @@ const Header = () => {
       <h2>
         <span className="secondary">Web</span> Developer
       </h2>
+      <a
+      href='https://github.com/21LukeOs/lukeosgood.com'
+      target="_blank"
+      rel="noopener noreferrer"
+      className='the-code'
+      >
+        <p className='the-code'>(https://github.com/21LukeOs/lukeosgood.com)</p>
+      </a>
     </header>
   );
 };
