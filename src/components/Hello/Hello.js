9d64da5ee38528390edc226afe4b6c136f9f6d89
Luke Osgood <lukemosgood@gmail.com>
2020-02-16 21:07:03 -0500

    fixed typo

--- src/components/Hello/Hello.js
+++ src/components/Hello/Hello.js
@@ -6,7 +6,7 @@ const Hello = () => {
 		<div className='box hello'>
 			<h3>Hello</h3>
 			<p>
-        Excited by and knowledgable about many vital technologies of the web such as HTML, CSS, Javascript, React, Git and more. Always challenging myself to grow as a developer and find ways to solve difficult problems. With the capacity to adapt and learn fast.
+        Excited by and knowledgeable about many vital technologies of the web such as HTML, CSS, Javascript, React, Git and more. Always challenging myself to grow as a developer and find ways to solve difficult problems. With the capacity to adapt and learn fast.
 			</p>
 		</div>
 	);
